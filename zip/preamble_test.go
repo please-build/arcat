62f95c1a3bef359bdc37fb30e0bfd725253ad91f
Chris Novakovic <chris@chrisn.me.uk>
2024-10-21 10:41:05 +0100

    `zip`: read all non-zip data from `--preamble_from` file (#22)
    The `zip` command's `--preamble_from` option, as used by Please's
    python-rules plugin, has strange semantics. The help text says it reads
    the first line from the given file and uses it as the preamble for the
    generated zip file, which is true unless the first line is `#!/bin/sh`,
    in which case the second line will also be read. The second line of
    "main" pex files generated by python-rules contain an `exec` command,
    allowing them to self-execute with more arguments than would otherwise
    be supported in a traditional shebang.
    
    Genericise the behaviour of `--preamble_from` so that it is useful for
    more than just files that begin with 1 or 2-line shell commands - assume
    the input is a zip file (a tacit assumption until now), and read all
    non-zip data from the start of the file and use it as the preamble for
    the generated zip file.
    
    Because python-rules' pex files are either pure zip files or zip files
    prepended with a 1 or 2-line shell command, this does not break
    backwards compatibility with python-rules.

---
+++ zip/preamble_test.go
@@ -0,0 +1,48 @@
+package zip
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPreamble(t *testing.T) {
+	for _, tc := range []struct{
+		ZipFile          string
+		PreambleChecksum string
+	}{
+		{
+			ZipFile:          "kitten_preamble.zip",
+			PreambleChecksum: "cd41527591eeac94f556fefa7505e54d9651e783cff0b8498ae0d65b55ffd416", // kitten.png
+		},
+		{
+			ZipFile:          "shebang_oneline_preamble.zip",
+			PreambleChecksum: "378f13f639d3ec2cbc18878b2d244adfaf4f9969464dd05aff0d5d9ed998d592", // shebang_oneline.txt
+		},
+		{
+			ZipFile:          "shebang_twolines_preamble.zip",
+			PreambleChecksum: "46533b2dfa35ad537d3561ebee0c7af8941bc65363c1b188e1be6eaf79e9138c", // shebang_twolines.txt
+		},
+		{
+			ZipFile:          "zip_preamble.zip",
+			PreambleChecksum: "038a57f3f807fa91bdd30239b9711fccf0d782fe2f036e03211852237e94d24c", // another.zip
+		},
+	} {
+		t.Run(tc.ZipFile, func(t *testing.T) {
+			r := require.New(t)
+
+			pr, err := Preamble("zip/test_data_4/" + tc.ZipFile)
+			r.NoError(err)
+			h := sha256.New()
+			_, err = io.Copy(h, pr)
+			r.NoError(err)
+			preambleChecksum := fmt.Sprintf("%x", h.Sum(nil))
+			r.Equal(tc.PreambleChecksum, preambleChecksum)
+			err = pr.Close()
+			r.NoError(err)
+		})
+	}
+}
